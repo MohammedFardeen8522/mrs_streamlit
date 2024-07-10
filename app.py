cfce4d398ba478c597f100dff47eed3f10752517
MohammedFardeen8522 <159554399+MohammedFardeen8522@users.noreply.github.com>
2024-07-10 20:40:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -35,24 +35,24 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names= recommend(selected_movie)
+    rd_movie_names, rd_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+        st.text(rd_movie_names[0])
+        st.image(rd_movie_posters[0])
     with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+        st.text(rd_movie_names[1])
+        st.image(rd_movie_posters[1])
 
     with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+        st.text(rd_movie_names[2])
+        st.image(rd_movie_posters[2])
     with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+        st.text(rd_movie_names[3])
+        st.image(rd_movie_posters[3])
     with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(rd_movie_names[4])
+        st.image(rd_movie_posters[4])
 
 
     # cols = st.columns(5)
